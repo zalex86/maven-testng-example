23642ceca6caf223491e176264509fd74cc8d5b9
Aleksandr <zalex_86@mail.ru>
2022-08-07 14:23:07 +0300

    Extended to parallel running

--- src/test/java/TeenageTestNGTest.java
+++ src/test/java/TeenageTestNGTest.java
@@ -1,30 +1,55 @@
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.testng.Assert.*;
 
 public class TeenageTestNGTest {
 
+    @BeforeClass
+    public void beforeClass() {
+        DateTimeFormatter dft = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dft.format(now));
+    }
+
+    @AfterClass
+    public void afterClass() {
+        DateTimeFormatter dft = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dft.format(now));
+    }
+
     @BeforeMethod
     public void setUp() {
+        System.out.println("TeenageTestNGTest test started");
     }
 
     @AfterMethod
     public void tearDown() {
+        System.out.println("TeenageTestNGTest test done");
     }
 
-    @DataProvider(name = "ageTest")
+    @DataProvider(name = "ageTest", parallel = true)
     Object[][] ageDataProvider() {
         return new Object[][] {
                 {10, true},
-                {20, false}
+                {20, false},
+                {30, false},
+                {40, false},
+                {50, false}
         };
     }
 
     @Test(dataProvider = "ageTest")
-    public void testIsTeenager(int age, boolean result) {
-        assertEquals(result, Teenage.isTeenager(age));
+    public void testIsTeenager(int age, boolean expectedResult) {
+        boolean result = Teenage.isTeenager(age);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals(result, expectedResult);
     }
 }
